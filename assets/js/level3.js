14f067c96536ea72964e7ccf06fbb176ceccac9b
Jacob Welsh <welshy92@outlook.com>
2023-05-15 09:02:30 +0000

    Spellcheck of JS and CSS completed

--- assets/js/level3.js
+++ assets/js/level3.js
@@ -528,7 +528,7 @@ function removeLifeIcon() {
   }
 }
 
-// Call the createLifeIcons() function to initialize the life icons
+// Call the createLifeIcons() function to initialise the life icons
 createLifeIcons();
 
 function gameOver() {
